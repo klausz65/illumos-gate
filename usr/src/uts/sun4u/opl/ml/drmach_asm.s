f2687c07dcc0df96a149d71fce946da18d9cad9d
Klaus Ziegler <klausz@haus-gisela.de>
2025-02-13 08:09:47 +0100

    Tribblix import: drmach-il-cpp fixes for M-Series servers

--- usr/src/uts/sun4u/opl/ml/drmach_asm.s
+++ usr/src/uts/sun4u/opl/ml/drmach_asm.s
@@ -48,6 +48,13 @@
 #include <sys/sbd_ioctl.h>
 
 /*
+ * From drmach.il.cpp
+ */
+#include <sys/sun4asi.h>
+#include <sys/machparam.h>
+#include <sys/opl_olympus_regs.h>
+
+/*
  * turn off speculative mode to prevent unwanted memory access
  * when we are in the FMEM loops
  */
@@ -465,3 +472,96 @@
 	retl
 	 nop
 	SET_SIZE(drmach_flush)
+
+/*
+ * From drmach.il.cpp
+ */
+/*
+ * Bcopy routine used by DR to copy
+ * between physical addresses.
+ * Borrowed from Starfire DR 2.6.
+ */
+	ENTRY_NP(bcopy32_il)
+	.register %g2, #scratch
+	.register %g3, #scratch
+	rdpr	%pstate, %g0
+	ldxa	[%o0]ASI_MEM, %o2
+	add	%o0, 8, %o0
+	ldxa	[%o0]ASI_MEM, %o3
+	add	%o0, 8, %o0
+	ldxa	[%o0]ASI_MEM, %g1
+	add	%o0, 8, %o0
+	ldxa	[%o0]ASI_MEM, %g2
+
+	stxa	%o2, [%o1]ASI_MEM
+	add	%o1, 8, %o1
+	stxa	%o3, [%o1]ASI_MEM
+	add	%o1, 8, %o1
+	stxa	%g1, [%o1]ASI_MEM
+	add	%o1, 8, %o1
+	stxa	%g2, [%o1]ASI_MEM
+
+	retl
+	nop
+	SET_SIZE(bcopy32_il)
+
+	ENTRY_NP(flush_cache_il)
+	rdpr	%pstate, %o3
+	andn	%o3, PSTATE_IE, %o4
+	wrpr	%g0, %o4, %pstate
+	mov	ASI_L2_CTRL_U2_FLUSH, %o4
+	mov	ASI_L2_CTRL_RW_ADDR, %o5
+	stxa	%o4, [%o5]ASI_L2_CTRL
+	! retl
+	wrpr	%g0, %o3, %pstate	! restore earlier pstate
+	SET_SIZE(flush_cache_il)
+
+	ENTRY_NP(drmach_get_stick_il)
+	retl
+	rd	STICK, %o0
+	SET_SIZE(drmach_get_stick_il)
+
+	ENTRY_NP(membar_sync_il)
+	retl
+	membar #Sync
+	SET_SIZE(membar_sync_il)
+/*
+ * flush_instr_mem:
+ *	Flush 1 page of the I-$ starting at vaddr
+ * 	%o0 vaddr
+ *
+ * SPARC64-VI maintains consistency of the on-chip Instruction Cache with
+ * the stores from all processors so that a FLUSH instruction is only needed
+ * to ensure pipeline is consistent. This means a single flush is sufficient at
+ * the end of a sequence of stores that updates the instruction stream to
+ * ensure correct operation.
+ */
+
+	ENTRY_NP(flush_instr_mem_il)
+	flush	%o0			! address irrelevant
+	retl
+	 nop
+	SET_SIZE(flush_instr_mem_il)
+
+/*
+ * drmach-sleep_il:
+ *
+ * busy loop wait can affect performance of the sibling strand
+ * the sleep instruction can be used to avoid that.
+ */
+
+	ENTRY_NP(drmach_sleep_il)
+.word	0x81b01060
+	retl
+	 nop
+	SET_SIZE(drmach_sleep_il)
+
+/*
+ * flush_windows_il:
+ *
+ */
+
+	ENTRY_NP(flush_windows_il)
+	retl
+	 flushw
+	SET_SIZE(flush_windows_il)
